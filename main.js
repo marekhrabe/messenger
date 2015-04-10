911edafbd8c217b8883a73f0d78c0d0e207de210
Anatoli Nicolae <nicolaeanatoli@gmail.com>
2015-04-10 18:33:24 +0200

    Improves Window design, opens links in default web browser (issue #1) and removes useless menu items (issue #3)

--- main.js
+++ main.js
@@ -1,21 +1,36 @@
-var app = require('app')
-var BrowserWindow = require('browser-window')
+var app = require('app');
+var ipc = require('ipc');
+var BrowserWindow = require('browser-window');
 
-var mainWindow = null
+var mainWindow = null;
 
+// App
 app.on('ready', function () {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    'accept-first-mouse': true,
-    'node-integration': false,
-    title: 'Loading',
-  })
+	//if (app.dock) app.dock.hide()
+	
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		frame: false,
+		resizable: false,
+		'web-preferences': {
+			plugins: true
+		},
+		title: 'Loading'
+	});
 
-  mainWindow.loadUrl('https://www.messenger.com')
+	mainWindow.loadUrl('file://' + __dirname + '/app.html');
+});
 
-  mainWindow.on('closed', function () {
-    mainWindow = null
-    app.quit()
-  })
-})
+// API
+ipc.on('do-native-action', function(event, action) {
+	switch (action) {
+		case 'quit':
+			app.quit();
+			break;
+			
+		case 'minimize':
+			mainWindow.minimize();
+			break;
+	}
+});
